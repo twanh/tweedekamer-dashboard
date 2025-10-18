0684cd6926b6b8d4f365f59fbc4a71237d89991c
tianniezing <78558772+tianniezing@users.noreply.github.com>
2025-10-18 16:51:01 +0200

    Flask app

---
+++ app/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+app = Flask(__name__)
+
+def get_db_results(query):
+    sparql = SPARQLWrapper("http://graphdb:7200/repositories/tk_repo")
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
+
+@app.route('/')
+def index():
+    # Example Query: Get all parties and their number of seats
+    query = """
+    PREFIX tk: <http://www.semanticweb.org/twanh/ontologies/2025/9/tk/>
+    SELECT ?fractieNaam ?aantalZetels
+    WHERE {
+        ?fractie a tk:Fractie ;
+                 tk:naam ?fractieNaam ;
+                 tk:aantalZetels ?aantalZetels .
+    }
+    """
+    results = get_db_results(query)
+    fracties = []
+    for result in results["results"]["bindings"]:
+        fracties.append({
+            'name': result['fractieNaam']['value'],
+            'seats': int(result['aantalZetels']['value'])
+        })
+    return render_template('index.html', fracties=fracties)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
