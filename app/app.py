fd06d00e7d8401d8d77bdc14d54c226e929cb047
Twan Huiskens <huiskenstwan@gmail.com>
2025-10-27 16:08:18 +0100

    Add page to show the agreement between parties (voting behaviour)

--- app/app.py
+++ app/app.py
@@ -45,6 +45,91 @@ def index():
     return render_template('index.html', fracties=fracties)
 
 
+@app.route('/agreement')
+def agreement():
+    """This page will show all the agreements between the parties in a cross table."""
+
+    query = """
+    PREFIX tk: <http://www.semanticweb.org/twanh/ontologies/2025/9/tk/>
+
+    SELECT
+        ?partyA_ab                 # Abbreviation for Party A
+        ?partyB_ab                 # Abbreviation for Party B
+        (COUNT(?zaak) AS ?commonVotes)  # Total votes where both parties voted 'voor' or 'tegen'
+        (SUM(IF(?voteA = ?voteB, 1, 0)) AS ?agreements) # Count of times they voted the same
+        (((SUM(IF(?voteA = ?voteB, 1, 0))) * 100.0 / COUNT(?zaak)) AS ?agreementPercentage)
+    WHERE {
+        ?zaak a tk:Zaak .
+
+        # Get Party A's vote
+        { ?partyA tk:heeftVoorGestemd ?zaak . BIND("voor" AS ?voteA) }
+        UNION
+        { ?partyA tk:heeftTegenGestemd ?zaak . BIND("tegen" AS ?voteA) }
+        # Get Party A's abbreviation
+        ?partyA a tk:Fractie ;
+                tk:afkorting ?partyA_ab .
+
+        # Get Party B's vote on the SAME 'zaak'
+        { ?partyB tk:heeftVoorGestemd ?zaak . BIND("voor" AS ?voteB) }
+        UNION
+        { ?partyB tk:heeftTegenGestemd ?zaak . BIND("tegen" AS ?voteB) }
+        # Get Party B's abbreviation
+        ?partyB a tk:Fractie ;
+                tk:afkorting ?partyB_ab .
+
+        # Filter for unique pairs to avoid calculating both A-B and B-A.
+        FILTER(?partyA_ab < ?partyB_ab)
+    }
+    # Group the results for each unique party pair
+    GROUP BY ?partyA_ab ?partyB_ab
+    # Order the final list alphabetically
+    ORDER BY ?partyA_ab ?partyB_ab
+    """
+
+    results = get_db_results(query)
+    agreements = []
+
+    for result in results['results']['bindings']:
+        agreements.append({
+            'partyA_ab': result['partyA_ab']['value'],
+            'partyB_ab': result['partyB_ab']['value'],
+            'commonVotes': result['commonVotes']['value'],
+            'agreements': result['agreements']['value'],
+            'agreementPercentage': result['agreementPercentage']['value'],
+        })
+
+    # Create a dictionary for quick lookup of agreement percentages
+    agreement_dict = {}
+    for agreement in agreements:
+        key1 = (agreement['partyA_ab'], agreement['partyB_ab'])
+        key2 = (agreement['partyB_ab'], agreement['partyA_ab'])
+        pct = float(agreement['agreementPercentage'])
+        agreement_dict[key1] = pct
+        agreement_dict[key2] = pct
+
+    # Get all unique parties
+    parties = set()
+    for agreement in agreements:
+        parties.add(agreement['partyA_ab'])
+        parties.add(agreement['partyB_ab'])
+    parties = sorted(list(parties))
+
+    # Create the agreement matrix
+    agreement_matrix = {}
+    for row_party in parties:
+        agreement_matrix[row_party] = {}
+        for col_party in parties:
+            if row_party == col_party:
+                agreement_matrix[row_party][col_party] = 100.0
+            else:
+                key = (row_party, col_party)
+                agreement_matrix[row_party][col_party] = agreement_dict.get(
+                    key, None,
+                )
+
+    return render_template('agreement.html', parties=parties, agreement_matrix=agreement_matrix)
+
+
 @app.route('/leden')
 def leden():
     # Example Query: Get all members and their parties
@@ -79,7 +164,7 @@ def leden():
 def fractie_detail(fractie_naam):
     # Decode the URL-encoded party name
     decoded_fractie_naam = unquote(fractie_naam)
-    
+
     # SPARQL query to get members and voting statistics for a specific party
     query = f"""
     PREFIX tk: <http://www.semanticweb.org/twanh/ontologies/2025/9/tk/>
@@ -89,11 +174,11 @@ def fractie_detail(fractie_naam):
       # Find the fractie by name
       ?fractie a tk:Fractie ;
                tk:naam "{decoded_fractie_naam}" .
-      
+
       # Get all members of this fractie
       ?fractie tk:heeftLid ?persoon .
       ?persoon tk:naam ?persoonNaam .
-      
+
       # OPTIONAL blocks to count votes in each category
       OPTIONAL {{ ?fractie tk:heeftVoorGestemd ?zaakVoor . }}
       OPTIONAL {{ ?fractie tk:heeftTegenGestemd ?zaakTegen . }}
@@ -102,15 +187,15 @@ def fractie_detail(fractie_naam):
     GROUP BY ?persoonNaam
     ORDER BY ?persoonNaam
     """
-    
+
     results = get_db_results(query)
-    
+
     leden = []
     # Initialize vote_counts; we only need one set of counts for the whole party
     vote_counts = {
         'voor': 0,
         'tegen': 0,
-        'niet_deelgenomen': 0
+        'niet_deelgenomen': 0,
     }
 
     bindings = results['results']['bindings']
@@ -120,17 +205,19 @@ def fractie_detail(fractie_naam):
         first_result = bindings[0]
         vote_counts['voor'] = int(first_result['stemmenVoor']['value'])
         vote_counts['tegen'] = int(first_result['stemmenTegen']['value'])
-        vote_counts['niet_deelgenomen'] = int(first_result['stemmenNietDeelgenomen']['value'])
-        
+        vote_counts['niet_deelgenomen'] = int(
+            first_result['stemmenNietDeelgenomen']['value'],
+        )
+
         # Get the list of all members
         for result in bindings:
             leden.append(result['persoonNaam']['value'])
 
     return render_template(
-        'fractie.html', 
-        fractie_naam=decoded_fractie_naam, 
+        'fractie.html',
+        fractie_naam=decoded_fractie_naam,
         leden=leden,
-        vote_counts=vote_counts  # Pass the vote counts to the template
+        vote_counts=vote_counts,  # Pass the vote counts to the template
     )
 
 
@@ -138,11 +225,11 @@ def fractie_detail(fractie_naam):
 def zaken():
     # Example Query: Get all zaken with their titles and onderwerpen
     query = """
-    SELECT ?zaak ?titel ?beschrijving ?besluitResultaat ?besluitStemmingsoort ?dossierNummer ?indieningsDatum 
+    SELECT ?zaak ?titel ?beschrijving ?besluitResultaat ?besluitStemmingsoort ?dossierNummer ?indieningsDatum
        ?isAfgedaan ?kabinetsappreciatie ?nummer ?termijn ?uuid ?volgnummer ?zaakSoort ?title
     WHERE {
     ?zaak a tk:Zaak .
-    
+
     OPTIONAL { ?zaak tk:titel ?titel . }
     OPTIONAL { ?zaak tk:beschrijving ?beschrijving . }
     OPTIONAL { ?zaak tk:besluitResultaat ?besluitResultaat . }
